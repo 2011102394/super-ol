e2fb998fa21ab90ffbc742f6ea6c8f9d1e801efa
zhangchaojia <602173141@qq.com>
2021-01-07 16:54:52 +0800

    feat: 修改webpack打包配置

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -7,17 +7,20 @@ module.exports = {
 		path: `${__dirname}/../dist/`,
 		libraryTarget: 'umd',
 	},
-	performance: {
-		hints: 'warning', // 枚举
-
-		maxAssetSize: 300000000000, // 整数类型（以字节为单位）
-
-		maxEntrypointSize: 50000000000, // 整数类型（以字节为单位）
-
-		assetFilter: function (assetFilename) {
-			// 提供资源文件名的断言函数
-
-			return assetFilename.endsWith('.css') || assetFilename.endsWith('.js')
-		},
+	externals: {
+		ol: 'ol',
 	},
+	// performance: {
+	// 	hints: 'warning', // 枚举
+
+	// 	maxAssetSize: 300000000000, // 整数类型（以字节为单位）
+
+	// 	maxEntrypointSize: 50000000000, // 整数类型（以字节为单位）
+
+	// 	assetFilter: function (assetFilename) {
+	// 		// 提供资源文件名的断言函数
+
+	// 		return assetFilename.endsWith('.css') || assetFilename.endsWith('.js')
+	// 	},
+	// },
 }
