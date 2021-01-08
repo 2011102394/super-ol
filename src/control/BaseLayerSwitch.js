93146af9bfa9422d75cb763be25d838d1778b063
zhangchaojia <602173141@qq.com>
2021-01-08 17:20:15 +0800

    feat: 完成底图切换组件封装

--- src/control/BaseLayerSwitch.js
+++ src/control/BaseLayerSwitch.js
@@ -7,29 +7,32 @@ const createBaseLayerSwitch = (initParam) => {
 	const container = document.getElementById(target)
 	const ids = []
 	baseLayers.forEach((item) => {
-		ids.push(item)
+		ids.push(item.id)
 	})
 	baseLayers.forEach((item) => {
 		const rowDom = document.createElement('div')
 		rowDom.setAttribute(
 			'style',
-			'display:flex;flex-direction: column;margin-left: 8px;margin-left: 8px;height:56px'
+			'display:flex;flex-direction:column;margin-left: 8px;margin-left: 8px;height:56px;font-size:12px;'
 		)
 		const imgDom = document.createElement('img')
 		imgDom.setAttribute('alt', item.displayName)
 		imgDom.setAttribute('title', item.displayName)
 		imgDom.setAttribute('src', item.avatar)
 		rowDom.appendChild(imgDom)
+		const titleDom = document.createElement('div')
+		titleDom.textContent = item.displayName
+		titleDom.setAttribute('style', 'text-align:center')
 		container.appendChild(rowDom)
-		addBaseLayer(map,item)
+		rowDom.appendChild(titleDom)
+		addBaseLayer(map, item)
 		rowDom.addEventListener('click', function () {
 			ids.forEach((id) => {
 				const layer = mapUtil.findLayerById(map, id)
-				console.log(layer)
-				if (item === id) {
-					layer.setVisiable(true)
+				if (item.id === id) {
+					layer.setVisible(true)
 				} else {
-					layer.setVisiable(false)
+					layer.setVisible(false)
 				}
 			})
 		})
@@ -38,19 +41,6 @@ const createBaseLayerSwitch = (initParam) => {
 
 const addBaseLayer = (map, layerOption) => {
 	mapUtil.addLayer(map, layerOption, layerOption.type)
-}
-
-function switchLayer(map, item) {
-	console.log(map)
-	console.log(item)
-	// ids.forEach((item) => {
-	// 	const layer = mapUtil.findLayerById(map, id)
-	// 	if (item === id) {
-	// 		layer.setVisiable(true)
-	// 	} else {
-	// 		layer.setVisiable(false)
-	// 	}
-	// })
 }
 
 export default {
